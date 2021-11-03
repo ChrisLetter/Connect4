18c91f8638665c21223fbdb18e1bd5be22d5264a
Christian Letter <christianletter@gmail.com>
2021-11-03 15:57:46 +0100

    feat(waiting for other users): styled "waiting for other users" and added spinner

--- client/src/components/WaitingForOtherUser.tsx
+++ client/src/components/WaitingForOtherUser.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Heading, Flex } from '@chakra-ui/react';
+import { Heading, Flex, Spinner } from '@chakra-ui/react';
 
 function WaitingForOtherUser(props: { [key: string]: string }) {
   const roomName = props.roomName;
 
   return (
-    <Heading
-      as="h1"
-      size="2xl"
-      fontWeight="bold"
-      color="teal.50"
-      textAlign={['center', 'center', 'center', 'center']}
-    >
-      Waiting for other user to connect... <br />
-      Tell your friend to join you at room "{roomName}"
-    </Heading>
+    <Flex direction="column" alignItems="center">
+      <Spinner
+        thickness="5px"
+        speed="1.65s"
+        emptyColor="teal.50"
+        color="teal.800"
+        size="xl"
+        mt="-20vh"
+        mb="5vh"
+      />
+      <Heading
+        as="h1"
+        size="2xl"
+        fontWeight="bold"
+        color="teal.50"
+        textAlign="center"
+        pb="4vh"
+      >
+        Waiting for other users to connect...
+      </Heading>
+      <Heading
+        as="h1"
+        size="2xl"
+        fontWeight="bold"
+        color="teal.50"
+        textAlign="center"
+      >
+        Tell your friends to join you at room "{roomName}"
+      </Heading>
+    </Flex>
   );
 }
 
